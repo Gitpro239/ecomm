39ea29b382c0dce0aceb7cdd833f275ef5320c5b
Gitpro239 <ahsan02390@gmail.com>
2025-03-19 05:42:54 +0000

    first commit

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -70,6 +70,7 @@ class Admin::StocksController < AdminController
 
     # Only allow a list of trusted parameters through.
     def admin_stock_params
-      params.expect(stock: [ :size, :amount, :product_id ])
+      params.require(:stock).permit(:size, :amount, :product_id)
+      # params.expect(stock: [ :size, :amount, :product_id ])
     end
 end
